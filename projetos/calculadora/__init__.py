8ca4a55a94adc463b9ae0bfb5d8ce9f5de8af26f
Danilo Nozima <danilo.nozima@gmail.com>
2023-03-30 11:21:17 -0300

    Adicionada atualização na função calcule

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -3,6 +3,20 @@ from funcoes import soma, subtracao, divisao, multiplicacao
 def calcule(): 
     a = input('Inpute um número a: ')
     b = input('Inpute um número b: ')
+    try: 
+        a = int(a)
+    except:
+        try:
+            a = float(a)
+        except: 
+            pass
+    try:
+        b = int(b)
+    except:
+        try:
+            b = float(b)
+        except:
+            pass
     operacao = input('Digite a operação desejada: soma (+), subtração (-), divisão (/) our multiplicação (*): ')
     if operacao == 'soma' or operacao == '+':
         soma(a, b)
